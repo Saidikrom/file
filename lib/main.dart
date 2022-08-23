42ce6236111453942ed9a16051bb489867ffe0ba
Saidikrom <yusupovsaidikrom9@gmail.com>
2022-08-23 16:37:55 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:download/download_button.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: DownloadButton(
+          url:
+              // 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+              'https://ftp1.optimedia.uz/123.mp4',
+        ),
+      ),
+    );
+  }
+}
